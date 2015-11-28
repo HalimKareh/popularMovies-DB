da3ec057b2228a07de750deb6d096bcab8a0e44a
HalimKareh <halim.a.kareh@gmail.com>
2015-11-28 22:30:42 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,65 +1,28 @@
-package com.example.halimkareh.popularmovies;
+package com.example.android.sunshine.app;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.GridView;
 
 
-public class MainActivity extends Activity {
-
-
-
-/*
-    public static TextView t1;
-    public static ImageView i1;
-
-    public static TextView t2;
-    public static ImageView i2;
-
-    public static TextView t3;
-    public static ImageView i3;
-*/
-    public static CustomGridViewAdapter customGridAdapter;
-    public static GridView movie_grid;
-    public static Context context;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*
-        movie_grid = (GridView)findViewById(R.id.movies_grid);
-
-        t1= (TextView)findViewById(R.id.text1);
-        i1 =(ImageView)findViewById(R.id.movie_poster_1);
-
-        t2= (TextView)findViewById(R.id.text2);
-        i2 =(ImageView)findViewById(R.id.movie_poster_2);
-
-        t3= (TextView)findViewById(R.id.text3);
-        i3 =(ImageView)findViewById(R.id.movie_poster_3);
-        */
-        String topRated="http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&&api_key=4d081fefd27c8fe3e0bbeb0ada19925e";
-        String APIKey = "4d081fefd27c8fe3e0bbeb0ada19925e";
-
-        movie_grid=(GridView)findViewById(R.id.movies_grid);
-        context=this;
-        // movie_poster_grid=(GridView)findViewById(R.id.movie_poster_grid);
-        moviesAsyncTask hello = new moviesAsyncTask();
-        hello.execute(topRated);
-
-
-
-     }
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MovieFragment())
+                    .commit();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -77,5 +40,9 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
 
 }
